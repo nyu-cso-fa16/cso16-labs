31646c156318dd719075cc6e377937abf080449f
jinyang <jinyang@cs.nyu.edu>
2016-11-22 12:10:54 -0500

    nits

---
+++ malloclab/mm.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+extern int mm_init (void);
+extern void *mm_malloc (size_t size);
+extern void mm_free (void *ptr);
+extern void *mm_realloc(void *ptr, size_t size);
+extern void mm_checkheap(int verbose);
+
+#define ALIGNMENT 16
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~(ALIGNMENT-1))
+
+/* Returns true if p is ALIGNMENT-byte aligned */
+#define IS_ALIGNED(p)  ((((long)(p)) % ALIGNMENT) == 0)
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* 
+ * Students work in teams of one or two.  Teams enter their team name, 
+ * personal names and login IDs in a struct of this
+ * type in their bits.c file.
+ */
+typedef struct {
+    char *teamname; /* ID1+ID2 or ID1 */
+    char *name1;    /* full name of first member */
+    char *id1;      /* github username of first member */
+    char *name2;    /* full name of second member (if any) */
+    char *id2;      /* github username of second member (if any)*/
+} team_t;
+
+extern team_t team;
+
